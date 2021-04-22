347dc849beddefe9478516639a910e97ddf58a7a
Ashley <ahappyday4all@gmail.com>
2021-04-22 14:54:28 +0800

    Add show page

--- app.js
+++ app.js
@@ -5,16 +5,26 @@ const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const port = 3000
 
-/* require express-handlebars */
+/* Require express-handlebars */
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 /* Route setting */
+// home page
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
-/* setting static files */
+// show page
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(
+    restaurant => restaurant.id === Number(req.params.restaurant_id)
+  )
+
+  res.render('show', { restaurant })
+})
+
+/* Setting static files */
 app.use(express.static('public'))
 
 /* Start and Listen on the server */
