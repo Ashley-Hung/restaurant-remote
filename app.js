9355c5ac9c6390616ff8db9f59b8e75232aae6bb
Ashley <ahappyday4all@gmail.com>
2021-04-23 13:22:42 +0800

    Add Search action

--- app.js
+++ app.js
@@ -9,10 +9,24 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+/* Setting static files */
+app.use(express.static('public'))
+
 /* Route setting */
 // home page
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
+})
+
+//search page
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter( restaurant =>
+      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      restaurant.name_en.toLowerCase().includes(keyword.toLowerCase())
+  )
+
+  res.render('index', { restaurants, keyword })
 })
 
 // show page
@@ -23,9 +37,6 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 
   res.render('show', { restaurant })
 })
-
-/* Setting static files */
-app.use(express.static('public'))
 
 /* Start and Listen on the server */
 app.listen(port, () => {
